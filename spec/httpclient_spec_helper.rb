df06212b58b29e6c442ad46de80ef41b86345451
Bartosz Blimke <bartosz@new-bamboo.co.uk>
2009-12-06 13:40:37 +0000

    HTTPClient asynchronous requests support

---
+++ spec/httpclient_spec_helper.rb
@@ -0,0 +1,55 @@
+module HTTPClientSpecHelper
+  attr_accessor :sync_mode
+  @sync_mode = true
+
+  def http_request(method, uri, options = {})
+    uri = Addressable::URI.heuristic_parse(uri)
+    c = HTTPClient.new
+    c.ssl_config.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    c.set_basic_auth(nil, uri.user, uri.password) if uri.user
+    params = [method, "#{uri.omit(:userinfo, :query).normalize.to_s}",
+      uri.query_values, options[:body], options[:headers] || {}]
+    if @sync
+      response = c.request(*params)
+    else
+      connection = c.request_async(*params)
+      response = connection.pop
+    end
+    OpenStruct.new({
+      :body => @sync ? response.content : response.content.read,
+      :headers => Hash[response.header.all],
+      :status => response.code.to_s })
+  end
+
+  def setup_expectations_for_real_request(options = {})
+    socket = mock("TCPSocket")
+    TCPSocket.should_receive(:new).
+      with(options[:host], options[:port]).at_least(:once).and_return(socket)
+
+    socket.stub!(:closed?).and_return(false)
+    socket.stub!(:close).and_return(true)
+
+    request_parts = ["#{options[:method]} #{options[:path]} HTTP/1.1", "Host: #{options[:host]}"]
+
+    if options[:port] == 443
+      OpenSSL::SSL::SSLSocket.should_receive(:new).
+        with(socket, instance_of(OpenSSL::SSL::SSLContext)).
+        at_least(:once).and_return(socket = mock("SSLSocket"))
+      socket.should_receive(:connect).at_least(:once).with()
+      socket.should_receive(:peer_cert).and_return(mock('peer cert', :extensions => []))
+      socket.should_receive(:write).with(/#{request_parts[0]}.*#{request_parts[1]}.*/m).and_return(100)
+    else
+      socket.should_receive(:<<).with(/#{request_parts[0]}.*#{request_parts[1]}.*/m).and_return(100)
+    end
+
+    socket.stub!(:sync=).with(true)
+
+    socket.should_receive(:gets).with("\n").once.and_return("HTTP/1.1 #{options[:response_code]} #{options[:response_message]}\nContent-Length: #{options[:response_body].length}\n\n#{options[:response_body]}")
+
+    socket.stub!(:eof?).and_return(true)
+    socket.stub!(:close).and_return(true)
+
+    socket.should_receive(:readpartial).any_number_of_times.and_raise(EOFError)
+  end
+
+end
--- spec/net_http_spec.rb
+++ spec/net_http_spec.rb
@@ -1,61 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require 'webmock_spec'
 require 'ostruct'
+require 'net_http_spec_helper'
+
+include NetHTTPSpecHelper
 
 describe "Webmock with Net:HTTP" do
-
-  def http_request(method, uri, options = {})
-    begin
-      uri = URI.parse(uri)
-    rescue
-      uri = Addressable::URI.heuristic_parse(uri)
-    end
-    response = nil
-    clazz = Net::HTTP.const_get("#{method.to_s.capitalize}")
-    req = clazz.new("#{uri.path}#{uri.query ? '?' : ''}#{uri.query}", options[:headers])
-
-    req.basic_auth uri.user, uri.password if uri.user
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true if uri.scheme == "https"
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    response = http.start {|http|
-      http.request(req, options[:body])
-    }
-    OpenStruct.new({
-      :body => response.body,
-      :headers => response,
-      :status => response.code })
-  end
-
-  # Sets several expectations that a real HTTP request makes it
-  # past WebMock to the socket layer. You can use this when you need to check
-  # that a request isn't handled by WebMock
-  #This solution is copied from FakeWeb project
-  def setup_expectations_for_real_request(options = {})
-    # Socket handling
-    if options[:port] == 443
-      socket = mock("SSLSocket")
-      OpenSSL::SSL::SSLSocket.should_receive(:===).with(socket).at_least(:once).and_return(true)
-      OpenSSL::SSL::SSLSocket.should_receive(:new).with(socket, instance_of(OpenSSL::SSL::SSLContext)).at_least(:once).and_return(socket)
-      socket.stub!(:sync_close=).and_return(true)
-      socket.should_receive(:connect).at_least(:once).with()
-    else
-      socket = mock("TCPSocket")
-      Socket.should_receive(:===).with(socket).at_least(:once).and_return(true)
-    end
-
-    TCPSocket.should_receive(:open).with(options[:host], options[:port]).at_least(:once).and_return(socket)
-    socket.stub!(:closed?).and_return(false)
-    socket.stub!(:close).and_return(true)
-
-    # Request/response handling
-    request_parts = ["#{options[:method]} #{options[:path]} HTTP/1.1", "Host: #{options[:host]}"]
-    socket.should_receive(:write).with(/#{request_parts[0]}.*#{request_parts[1]}.*/m).and_return(100)
-
-    socket.should_receive(:sysread).once.and_return("HTTP/1.1 #{options[:response_code]} #{options[:response_message]}\nContent-Length: #{options[:response_body].length}\n\n#{options[:response_body]}")
-    socket.should_receive(:sysread).any_number_of_times.and_raise(EOFError)
-  end
-
 
   it_should_behave_like "WebMock"
 
