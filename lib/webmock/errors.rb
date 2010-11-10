a700193b516ee497f981252925587aebe3073359
Bartosz Blimke <bartosz.blimke@gmail.com>
2010-11-10 21:55:34 +0000

    Extracted stubbing instructions to a separate method to make it easy to override them.

--- lib/webmock/errors.rb
+++ lib/webmock/errors.rb
@@ -3,11 +3,18 @@ module WebMock
   class NetConnectNotAllowedError < StandardError
     def initialize(request_signature)
       text = "Real HTTP connections are disabled. Unregistered request: #{request_signature}"
-      text << "\n\n"
+      text << stubbing_instructions(request_signature)
+      super(text)
+    end
+
+    private
+
+    def stubbing_instructions(request_signature)
+      text = "\n\n"
       text << "You can stub this request with the following snippet:\n\n"
       text << WebMock::StubRequestSnippet.new(request_signature).to_s
       text << "\n\n" + "="*60
-      super(text)
+      text
     end
   end
 
