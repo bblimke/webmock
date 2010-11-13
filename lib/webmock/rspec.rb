9d255f118a6a39d297856fa83302aca1577b2c03
Bartosz Blimke <bartosz.blimke@gmail.com>
2010-11-13 00:30:07 +0000

    Fixed issue with `webmock/rspec` which didn't load correctly if `rspec/core` was already required but `rspec/expectations` not.

--- lib/webmock/rspec.rb
+++ lib/webmock/rspec.rb
@@ -1,14 +1,15 @@
 require 'webmock'
 
 # RSpec 1.x and 2.x compatibility
-if defined?(RSpec)
+if defined?(RSpec) && defined?(RSpec::Expectations)
   RSPEC_NAMESPACE = RSPEC_CONFIGURER = RSpec
 elsif defined?(Spec)
   RSPEC_NAMESPACE = Spec
   RSPEC_CONFIGURER = Spec::Runner
 else  
   begin
-    require 'rspec'
+    require 'rspec/core'
+    require 'rspec/expectations'
     RSPEC_NAMESPACE = RSPEC_CONFIGURER = RSpec
   rescue LoadError
     require 'spec'
