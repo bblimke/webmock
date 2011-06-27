643af27dc616b1916e6f0f435a8bd87fbaf1f69d
phiggins <pete@peterhiggins.org>
2011-06-26 19:00:40 -0700

    Tweak regexp used in Patron adapter to work around breakage (apparently) from last commit.

--- lib/webmock/http_lib_adapters/patron.rb
+++ lib/webmock/http_lib_adapters/patron.rb
@@ -87,7 +87,7 @@ if defined?(Patron)
 
       def build_webmock_response(patron_response)
         webmock_response = WebMock::Response.new
-        reason = patron_response.status_line.scan(%r(\AHTTP/(\d+\.\d+)\s+(\d\d\d)\s*([^\r\n]+)?\r?\z))[0][2]
+        reason = patron_response.status_line.scan(%r(\AHTTP/(\d+\.\d+)\s+(\d\d\d)\s*([^\r\n]+)?\r?))[0][2]
         webmock_response.status = [patron_response.status, reason]
         webmock_response.body = patron_response.body
         webmock_response.headers = patron_response.headers
