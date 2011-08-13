c3121c531e32fac7c2727480d13c270c7e78aa85
Bartosz Blimke <bartosz.blimke@gmail.com>
2011-08-14 01:36:34 +0200

    Increaset Typhoeus timeout to 10s in webmock specs

--- spec/typhoeus_hydra_spec_helper.rb
+++ spec/typhoeus_hydra_spec_helper.rb
@@ -11,7 +11,7 @@ module TyphoeusHydraSpecHelper
         :method  => method,
         :body    => options[:body],
         :headers => options[:headers],
-        :timeout => 5000 # milliseconds
+        :timeout => 10000 # milliseconds
       }
     )
     raise FakeTyphoeusHydraError.new if response.code.to_s == "0"
