413eedca7da99270ac83e931b59b56a0c9600e0a
Bartosz Blimke <bartosz.blimke@gmail.com>
2011-08-13 21:46:46 +0200

    Fixed file formatting

--- lib/webmock/http_lib_adapters/http_lib_adapter_registry.rb
+++ lib/webmock/http_lib_adapters/http_lib_adapter_registry.rb
@@ -3,15 +3,15 @@ module WebMock
     include Singleton
 
     attr_accessor :http_lib_adapters
-    
+
     def initialize
       @http_lib_adapters = {}
     end
-    
+
     def register(lib, adapter)
       @http_lib_adapters[lib] = adapter
     end
-    
+
     def each_adapter(&block)
       @http_lib_adapters.values.each(&block)
     end
