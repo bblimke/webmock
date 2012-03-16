b5213286ae10f280f1d7b82306e3c7b829cf813b
Bartosz Blimke <bartosz.blimke@gmail.com>
2012-03-16 23:58:03 +0100

    Don't set ssl_timeout for Net::HTTP in jruby

--- spec/acceptance/net_http/net_http_spec_helper.rb
+++ spec/acceptance/net_http/net_http_spec_helper.rb
@@ -23,7 +23,7 @@ module NetHTTPSpecHelper
     if uri.scheme == "https"
       http.use_ssl = true
       #1.9.1 has a bug with ssl_timeout
-      http.ssl_timeout = 20 if http.respond_to?("ssl_timeout=")
+      http.ssl_timeout = 20 unless RUBY_PLATFORM =~ /java/
       http.open_timeout = 60
       http.read_timeout = 60
     end
