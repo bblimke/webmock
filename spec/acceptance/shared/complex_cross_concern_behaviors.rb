fb41a00525afaf745531b0d22bc12db13d96315d
Bartosz Blimke <bartosz.blimke@gmail.com>
2012-07-24 01:05:15 +0200

    Acceptance spec incompatible with Curb doesn't run in Curb specs.

--- spec/acceptance/shared/complex_cross_concern_behaviors.rb
+++ spec/acceptance/shared/complex_cross_concern_behaviors.rb
@@ -21,13 +21,15 @@ shared_context "complex cross-concern behaviors" do |*adapter_info|
   let(:no_content_url) { 'http://httpstat.us/204' }
   [nil, ''].each do |stub_val|
     it "returns the same value (nil or "") for a request stubbed as #{stub_val.inspect} that a real empty response has" do
-      WebMock.allow_net_connect!
+      unless http_library == :curb
+        WebMock.allow_net_connect!
 
-      real_response = http_request(:get, no_content_url)
-      stub_request(:get, no_content_url).to_return(:status => 204, :body => stub_val)
-      stubbed_response = http_request(:get, no_content_url)
+        real_response = http_request(:get, no_content_url)
+        stub_request(:get, no_content_url).to_return(:status => 204, :body => stub_val)
+        stubbed_response = http_request(:get, no_content_url)
 
-      stubbed_response.body.should eq(real_response.body)
+        stubbed_response.body.should eq(real_response.body)
+      end
     end
   end
 end
