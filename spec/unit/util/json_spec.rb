740284b7424f97283040f7f253597e7cedf8bb6e
Levin Alexander <mail@levinalex.net>
2013-03-07 16:15:07 +0100

    failing test for JSON parser

--- spec/unit/util/json_spec.rb
+++ spec/unit/util/json_spec.rb
@@ -4,4 +4,8 @@ describe WebMock::Util::JSON do
   it "should parse json without parsing dates" do
     WebMock::Util::JSON.parse("\"a\":\"2011-01-01\"").should == {"a" => "2011-01-01"}
   end
+
+  it "should parse json that includes binary strings" do
+    WebMock::Util::JSON.parse("{\"a\":\"\\u0000\\u0001\\u0002\"}").should == {"a" => "\x00\x01\x02"}
+  end
 end
