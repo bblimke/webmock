30d8e01b12525bd8686f583715d7f406335fdd66
Tim Kurvers <tim@moonsphere.net>
2013-05-17 23:38:25 +0200

    Use accessor for MiniTest assertions count

--- lib/webmock/minitest.rb
+++ lib/webmock/minitest.rb
@@ -14,7 +14,7 @@ MiniTest::Test.class_eval do
   [:assert_request_requested, :assert_request_not_requested].each do |name|
     alias_method :"#{name}_without_assertions_count", name
     define_method :"#{name}_with_assertions_count" do |*args|
-      self._assertions += 1
+      self.assertions += 1
       send :"#{name}_without_assertions_count", *args
     end
     alias_method name, :"#{name}_with_assertions_count"
