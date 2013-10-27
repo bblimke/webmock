1dc09899039e4a2e563853d903091f1b5d9de659
Tamir Duberstein <tamird@squareup.com>
2013-10-27 12:41:27 -0700

    Fix rspec deprecation warnings

--- spec/unit/errors_spec.rb
+++ spec/unit/errors_spec.rb
@@ -29,11 +29,11 @@ describe "errors" do
       end
 
       it "should not be caught by a rescue block without arguments" do
-        request_signature = mock(:to_s => "aaa")
-        request_stub = mock
-        WebMock::RequestStub.stub!(:from_request_signature).and_return(request_stub)
-        WebMock::StubRequestSnippet.stub!(:new).
-          with(request_stub).and_return(mock(:to_s => "bbb"))
+        request_signature = double(:to_s => "aaa")
+        request_stub = double
+        WebMock::RequestStub.stub(:from_request_signature).and_return(request_stub)
+        WebMock::StubRequestSnippet.stub(:new).
+          with(request_stub).and_return(double(:to_s => "bbb"))
 
         exception = WebMock::NetConnectNotAllowedError.new(request_signature)
 
