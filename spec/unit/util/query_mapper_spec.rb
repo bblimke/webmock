963abd8fbb2880f8eb27294b297de372cc7fc767
Alexander Simonov <alex@simonov.me>
2013-12-12 21:15:26 +0200

    Fix for #337 with refactoring and more tests

--- spec/unit/util/query_mapper_spec.rb
+++ spec/unit/util/query_mapper_spec.rb
@@ -1,30 +1,41 @@
 require 'spec_helper'
 
 describe WebMock::Util::QueryMapper do
-  let(:query_mapper) { described_class }
-
-  it "converts query to values" do
-    query = "key=value&other_key=other_value"
-    values = { 'key' => 'value', 'other_key' => 'other_value' }
-    expect(query_mapper.query_to_values query).to eq values
+  it "should parse hash queries" do
+    # {"one" => {"two" => {"three" => ["four", "five"]}}}
+    query = "one%5Btwo%5D%5Bthree%5D%5B%5D=four&one%5Btwo%5D%5Bthree%5D%5B%5D=five"
+    hsh = WebMock::Util::QueryMapper.query_to_values(query)
+    hsh["one"]["two"]["three"].should == ["four", "five"]
   end
 
-  it 'converts values to a query string' do
-    query = "key=value&other_key=other_value"
-    values = [['key','value'],['other_key','other_value']]
-    expect(query_mapper.values_to_query values).to eq query
+  it "should parse one nil value queries" do
+    # {'a' => nil, 'b' => 'c'}
+    query = "a=&b=c"
+    hsh = WebMock::Util::QueryMapper.query_to_values(query)
+    hsh['a'].should be_empty
+    hsh['b'].should == 'c'
   end
 
-  it 'converts values with missing keys to a query string' do
-    query = "=value"
-    values = { '' => 'value' }
-    expect(query_mapper.values_to_query values).to eq query
+  it "should parse array queries" do
+    # {"one" => ["foo", "bar"]}
+    query = "one%5B%5D=foo&one%5B%5D=bar"
+    hsh = WebMock::Util::QueryMapper.query_to_values(query)
+    hsh["one"].should == ["foo", "bar"]
   end
 
-  it 'converts values with nil keys to a query string' do
-    query = "=value"
-    values = { nil => 'value' }
-    expect(query_mapper.values_to_query values).to eq query
+  it "should parse string queries" do
+    # {"one" => "two", "three" => "four"}
+    query = "one=two&three=four"
+    hsh = WebMock::Util::QueryMapper.query_to_values(query)
+    hsh.should == {"one" => "two", "three" => "four"}
+  end
+
+  it "should parse nested queries" do
+    # [{"b"=>[{"c"=>[{"d"=>["1", {"e"=>"2"}]}]}]}]
+    query = "a%5B%5D%5Bb%5D%5B%5D%5Bc%5D%5B%5D%5Bd%5D%5B%5D=1&a%5B%5D%5Bb%5D%5B%5D%5Bc%5D%5B%5D%5Bd%5D%5B%5D%5Be%5D=2"
+    hsh = WebMock::Util::QueryMapper.query_to_values(query)
+    hsh["a"][0]["b"][0]["c"][0]["d"][0].should == "1"
+    hsh["a"][0]["b"][0]["c"][0]["d"][1]["e"].should == "2"
   end
 
 end
