66e051b85ecdecb59fbb60bcda8a5942cfb006c0
Aren Patel <aren55555@users.noreply.github.com>
2014-09-18 23:47:30 -0700

    Fixed the calling of normalize twice in the case where the request had basic Auth.

--- lib/webmock/util/uri.rb
+++ lib/webmock/util/uri.rb
@@ -17,7 +17,7 @@ module WebMock
           sorted_query_values = sort_query_values(WebMock::Util::QueryMapper.query_to_values(normalized_uri.query) || {})
           normalized_uri.query = WebMock::Util::QueryMapper.values_to_query(sorted_query_values)
         end
-        normalized_uri = normalized_uri.normalize #normalize! is slower
+        normalized_uri = normalized_uri.normalize if !(normalized_uri.class == Addressable::URI && normalized_uri.userinfo.present?) #normalize! is slower
         normalized_uri.query = normalized_uri.query.gsub("+", "%2B") if normalized_uri.query
         normalized_uri.port = normalized_uri.inferred_port unless normalized_uri.port
         hash[uri] = normalized_uri
