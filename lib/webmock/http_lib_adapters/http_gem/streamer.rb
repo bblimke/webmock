7befb72619c38ffe00ddb5a81d595449c2a03abd
Zach Anker <zanker@squareup.com>
2015-03-26 21:23:08 -0700

    Fixed Http.rb mock to set sequence_id

--- lib/webmock/http_lib_adapters/http_gem/streamer.rb
+++ lib/webmock/http_lib_adapters/http_gem/streamer.rb
@@ -5,8 +5,20 @@ module HTTP
         @io = StringIO.new str
       end
 
-      def readpartial(size = HTTP::Client::BUFFER_SIZE)
+      def readpartial(size = nil)
+        unless size
+          if defined?(HTTP::Client::BUFFER_SIZE)
+            size = HTTP::Client::BUFFER_SIZE
+          elsif defined?(HTTP::Connection::BUFFER_SIZE)
+            size = HTTP::Connection::BUFFER_SIZE
+          end
+        end
+
         @io.read size
+      end
+
+      def sequence_id
+        -1
       end
     end
   end
