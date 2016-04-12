9fd5a9013da7f146ada92657b202492837d76107
Bartosz Blimke <bartosz.blimke@gmail.com>
2016-04-12 15:03:01 +0200

    Much faster way of detecting network connectivity.

--- spec/support/network_connection.rb
+++ spec/support/network_connection.rb
@@ -1,22 +1,16 @@
-module NetworkConnection
-  def self.connect_to(host, port, timeout=nil)
-    addr = Socket.getaddrinfo(host, nil)
-    sock = Socket.new(Socket.const_get(addr[0][0]), Socket::SOCK_STREAM, 0)
+require 'timeout'
+require 'socket'
 
-    if timeout
-      secs = Integer(timeout)
-      usecs = Integer((timeout - secs) * 1_000_000)
-      optval = [secs, usecs].pack("l_2")
-      sock.setsockopt Socket::SOL_SOCKET, Socket::SO_RCVTIMEO, optval
-      sock.setsockopt Socket::SOL_SOCKET, Socket::SO_SNDTIMEO, optval
+module NetworkConnection
+  def self.connect_to(host, port, timeout=10)
+    timeout(timeout) do
+      TCPSocket.new(host, port)
     end
-    sock.connect(Socket.pack_sockaddr_in(port, addr[0][3]))
-    sock
   end
 
   def self.is_network_available?
     begin
-      self.connect_to("173.194.113.176", 80, 5)
+      self.connect_to("8.8.8.8", 53, 5)
       true
     rescue
       false
