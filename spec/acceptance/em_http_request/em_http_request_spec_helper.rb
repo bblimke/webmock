f38c47e90df6584732781f33e53941e74086f565
Tadas Sce <tadas@sce.lt>
2019-08-01 17:08:06 +0100

    Fix EM HTTP request adapter's timeout class
    As defined in a test, that's the expected error class:
    https://github.com/igrigorik/em-http-request/blob/907f46713e8257e1c1dd348bc5da77adc8d38392/spec/client_spec.rb#L499

--- spec/acceptance/em_http_request/em_http_request_spec_helper.rb
+++ spec/acceptance/em_http_request/em_http_request_spec_helper.rb
@@ -50,7 +50,7 @@ module EMHttpRequestSpecHelper
   end
 
   def client_timeout_exception_class
-    "WebMock timeout error"
+    'Errno::ETIMEDOUT'
   end
 
   def connection_refused_exception_class
