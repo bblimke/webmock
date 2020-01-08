a490f6a8cff3d5d96cad38f6ab4bde9e36ffa5c5
Tom Brammar <tbrammar@premiacapital.com>
2020-01-08 09:13:08 +0100

    more descriptive method name

--- lib/webmock/http_lib_adapters/typhoeus_hydra_adapter.rb
+++ lib/webmock/http_lib_adapters/typhoeus_hydra_adapter.rb
@@ -102,7 +102,7 @@ if defined?(Typhoeus)
               status_message: webmock_response.status[1],
               body: webmock_response.body,
               headers: webmock_response.headers,
-              effective_url: effective_url(request_signature.uri)
+              effective_url: standardise_stringify_uri(request_signature.uri)
             )
           end
           response.mock = :webmock
@@ -171,7 +171,7 @@ if defined?(Typhoeus)
 
         private
 
-        def self.effective_url(uri)
+        def self.standardise_stringify_uri(uri)
           return uri.omit(:port).to_s if [80, 443].include?(uri.port)
           uri.to_s
         end
