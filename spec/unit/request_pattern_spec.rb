bcf38968fe3885cc21db1c9f97ab3402733ef641
Rodrigo Argumedo <7613139+rodrigoargumedo@users.noreply.github.com>
2024-09-10 21:29:35 +0000

    Fix test

--- spec/unit/request_pattern_spec.rb
+++ spec/unit/request_pattern_spec.rb
@@ -614,7 +614,7 @@ describe WebMock::RequestPattern do
             it "should not match when body is not xml" do
               expect(WebMock::RequestPattern.new(:post, 'www.example.com', body: body_hash)).
                 not_to match(WebMock::RequestSignature.new(:post, "www.example.com",
-                                                               headers: {content_type: content_type}, body: "foo bar"))
+                                                               headers: {content_type: content_type}, body: ""))
                 end
 
             it "matches when the content type include a charset" do
