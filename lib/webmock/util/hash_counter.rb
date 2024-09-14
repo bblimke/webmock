6890a013150d63fd18413d005c89b161fd5d9ac9
John Gallagher <john@synapticmishap.co.uk>
2024-09-14 15:15:38 +0200

    Deal with recycled objects

--- lib/webmock/util/hash_counter.rb
+++ lib/webmock/util/hash_counter.rb
@@ -29,6 +29,8 @@ module WebMock
         request_object_id = key.object_id if request_object_id.nil?
         num.times do
           array << ObjectSpace._id2ref(request_object_id)
+        rescue RangeError
+          # Points to an invalid or recycled object so ignore
         end
         @request_object_ids[key.hash] = key.object_id
       end
